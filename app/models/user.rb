6929f83c31b3628546bd848dcd0fd8bb492c9497
ellisandrews <ellisandrews1@gmail.com>
2020-03-19 13:09:24 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :hosts, :class_name => "User"
+  has_many :guests, :class_name => "User"
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
